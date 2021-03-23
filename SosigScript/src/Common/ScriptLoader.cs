395fa1f9e16bb569922ce763b8939aecc534dcb7
Frityet <ambhogal01@gmail.com>
2021-03-22 23:13:17 -0700

    dw

--- SosigScript/src/Common/ScriptLoader.cs
+++ SosigScript/src/Common/ScriptLoader.cs
@@ -1,7 +1,42 @@
-﻿namespace SosigScript.Loaders
+﻿using System.Collections.Generic;
+using System.IO;
+using BepInEx.Logging;
+using Deli;
+using Deli.Patcher;
+using Deli.Setup;
+using Deli.VFS;
+
+namespace SosigScript.Common
 {
-    public class ScriptLoader
+    public class ScriptLoader 
     {
+
+        public Dictionary<string, string> Scripts => _modInfo;
+
+        private int _loaded;
+
+        private Dictionary<string, string> _modInfo = new();
         
+        public void Load(SetupStage stage, Mod mod, IHandle handle)
+        {
+
+            if (handle is IFileHandle file)
+            {
+                _modInfo.Add(mod.Info.Guid, stage.ImmediateReaders.Get<string>()(file));
+            }
+                
+        }
+        
+        /*
+
+        public void OnPatcher(PatcherStage stage) => 
+            stage.PatcherAssetLoaders
+            [
+                ScriptLoader,
+                "SosigScript"
+            ] = Load; 
+        
+        */
     }
+    
 }
